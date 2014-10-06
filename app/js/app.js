c3b301df829cb20792170b6351fbe8114d1b0002
Bryann Lemoigne <lemoigne@fullsix.com>
2014-10-06 02:42:11 +0200

    modif menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,18 @@
 $(document).foundation();
 
 
+$(document).ready(function(){
+    $(".actionMenu").bind("click", function(e){
+        e.preventDefault();
+        var menu = $("#menu");
+
+
+        if (menu.hasClass("active")) {
+            menu.removeClass("active");
+        } else {
+            menu.addClass("active");
+        }
+
+    });
+});
 
